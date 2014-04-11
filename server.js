74c2e698fdfb7b1e044b556999eabb2879320998
Thomas Davis <thomasalwyndavis@gmail.com>
2014-04-11 15:44:23 +1000

    for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
