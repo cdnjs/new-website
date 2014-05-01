a22521fffecb0a8db86832bbabb97789ebccd80d
Thomas Davis <thomasalwyndavis@gmail.com>
2014-05-01 05:51:53 -0400

    build

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Exit if any errors
+set -e
+cd /root/new-website
+echo Getting latest website changes...
+git pull
+
+echo npm install for good mesure
+/usr/local/bin/npm install
+
+echo Re-generating website...
+/usr/local/bin/node update.js
+
+echo Uploading website...
+git add .
+git commit -am "Updated packages."
+git push
+git push heroku master
