2ba7923dd904cd09f9740fe259ac8f709913fbec
Thomas Davis <thomasalwyndavis@gmail.com>
2014-05-16 13:49:28 +1000

    track.io broke login

--- public/js/main.js
+++ public/js/main.js
@@ -433,4 +433,41 @@ $('body').on('click', '.remove-favorite', function(e) {
     })
     return false;
   });
+
+  // Function to show and hide the loader anim
+  function showLoader(show) {
+    document.getElementById("loader").style.display = (show ? "block" : "none");
+  }
+  $('.login-form').on('submit', function (ev) {
+  // Login the user
+    // Show the loader
+    showLoader(true);
+
+    // This will authenticate the user
+    UserApp.User.login({
+      login: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    }, function(error, result) {
+      if (error) {
+        // Wrong password maybe?
+        alert("Error: " + error.message);
+        showLoader(false);
+      } else {
+        
+        onLoginSuccessful();
+      }
+    });
+    // When the user has been logged in successfully
+    function onLoginSuccessful() {
+      // Now, save the token in a cookie
+      Kaka.set("ua_session_token", UserApp.global.token);
+
+      // Redirect the user to the index page
+      window.location.href = "/";
+      showLoader(false);
+    }
+    return false;
+
+  })
+
 })(jQuery);
