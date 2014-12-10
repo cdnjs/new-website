ab92c64065c251934843b1d50479e6048cbb6106
Sylvain UTARD <sylvain.utard@gmail.com>
2014-12-09 17:50:23 -0800

    Improve search relevance with GithHub  - Use GitHub's API to enrich the libraries  - Update the index settings to use the stargazer count as popularity

--- reindex.js
+++ reindex.js
@@ -1,48 +1,167 @@
 var _ = require("lodash");
 var fs = require("fs");
 var AlgoliaSearch = require("algolia-search");
+var GitHubApi = require("github");
+var async = require("async");
 
-// fetch all libraries from the generated public/packages.json file
-var LIBRARIES = _.map(JSON.parse(fs.readFileSync('public/packages.json', 'utf8')).packages, function(library) {
-  library.originalName = library.name;
-  library.name = library.name.toLowerCase();
-  library.objectID = library.name.replace(/\./g, '');
-  // add some alternative name forms to improve the search relevance
-  library.alternativeNames = [
-    library.name.split(/[^a-zA-Z]/).join(''),         // font-awesome <=> fontawesome
-    library.name.replace(/([a-z](?=[A-Z]))/g, '$1 ')  // camelCase <=> camel case
-  ];
-  if(library.filename && library.filename.substr(library.filename.length-3, library.filename.length) === 'css') {
-    library.fileType = 'css';
-  } else {
-    library.fileType = 'js';
-  }
-  delete library.assets;
-  return library;
-});
 
-// init algolia API client
+/*
+ * This script is in charge of the libraries indexing
+ * It makes an intensive use of the `async` library to ease the asynchronous processing.
+ *
+ * Here is how it works:
+ *  - fetch the libraries from public/packages.json
+ *  - retrieve various infos from GitHub (number of stars, etc..)
+ *    - create an oauth token
+ *    - authenticate with the generated token
+ *    - for each library
+ *      - try to find a github.com URL in the library's configuration
+ *      - fetch the associated repository stats
+ *      - enrich the library with various infos
+ *  - push the libraries to Algolia
+ *    - init+configure a temporary index
+ *    - push libraries to the temporary index
+ *    - move the temporary index to the production index (atomic)
+ *
+*/
+
+//////
+////// Fetch all libraries from the generated public/packages.json file
+//////
+var LIBRARIES = [];
+function load(next) {
+  console.log('* Loading libraries');
+  LIBRARIES = _.map(JSON.parse(fs.readFileSync('public/packages.json', 'utf8')).packages, function(library) {
+    library.originalName = library.name;
+    library.name = library.name.toLowerCase();
+    library.objectID = library.name.replace(/\./g, '');
+    // add some alternative name forms to improve the search relevance
+    library.alternativeNames = [
+      library.name.split(/[^a-zA-Z]/).join(''),         // font-awesome <=> fontawesome
+      library.name.replace(/([a-z](?=[A-Z]))/g, '$1 ')  // camelCase <=> camel case
+    ];
+    if(library.filename && library.filename.substr(library.filename.length-3, library.filename.length) === 'css') {
+      library.fileType = 'css';
+    } else {
+      library.fileType = 'js';
+    }
+    delete library.assets;
+    return library;
+  });
+  next();
+}
+
+//////
+////// Enrich libraries with GitHub stats
+//////
+var github = new GitHubApi({ version: "3.0.0" });
+
+function authenticate(next) {
+  github.authenticate({
+    type: "oauth",
+    token: '2e25fb969a43b6470e76ee9e75f919ce03003afb' // scope=public_repos only (not secret)
+  });
+  next();
+}
+
+function crawl(gnext) {
+  console.log('* Enriching libraries with GitHub');
+  async.each(LIBRARIES, function(library, next) {
+    var urls = [];
+    // collect all repository urls
+    if (library.repositories && _.isArray(library.repositories)) {
+      for (var i = 0; i < library.repositories.length; ++i) {
+        urls.push(library.repositories[i].url);
+      }
+    }
+    if (library.repository) {
+      if (_.isObject(library.repository)) {
+        urls.push(library.repository.url);
+      } else if (_.isString(library.repository)) {
+        urls.push(library.repository);
+      }
+    }
+    // for each collected URL, try to find the associated github repo
+    var repos = _.compact(_.map(urls, function(url) {
+      if (!url || !_.isString(url)) {
+        return null;
+      }
+      var m = url.match(/.*github\.com\/([^\/]+)\/([^\/]+)(\/.*|\.git)?$/);
+      if (!m) {
+        return null;
+      }
+      return { user: m[1], repo: m[2].replace(/.git$/, '') };
+    }));
+
+    if (repos.length > 0) {
+      var repo = repos[0]; // fetch only the first repository
+      github.repos.get(repo, function(err, res) {
+        if (!err) {
+          // enchrich the library
+          console.log('** Enrich ' + repo.user + '/' + repo.repo);
+          library.github = {
+            user: repo.user,
+            repo: repo.repo,
+            stargazers_count: res.stargazers_count,
+            watchers_count: res.watchers_count,
+            forks: res.forks,
+            open_issues_count: res.open_issues_count,
+            subscribers_count: res.subscribers_count
+          }
+        }
+        next();
+      });
+    } else {
+      next();
+    }
+  }, gnext);
+}
+
+//////
+////// Push libraries to Algolia for the indexing
+//////
 var client = new AlgoliaSearch('2QWLVLXZB6', process.env.ALGOLIA_API_KEY);
-
-console.log('Initializing the index');
 var index = client.initIndex('libraries.tmp');
-index.setSettings({
-  attributesToIndex: ['name', 'unordered(alternativeNames)', 'unordered(description)', 'keywords', 'filename'],
-  customRanking: ['asc(name)'], // FIXME: having the number of downloads or something
-                                //        reflecting the popularity could be cool
-  attributesForFaceting: ['fileType', 'keywords'],
-  optionalWords: ['js', 'css'] // those words are optional (jquery.colorbox.js <=> jquery.colorbox)
-});
 
-console.log('Indexing ' + LIBRARIES.length + ' libraries');
-index.addObjects(LIBRARIES, function(error, content) {
-  if (error) {
-    console.log('Error:' + content.message);
+function initIndex(next) {
+  console.log('* Initializing the index');
+  index.setSettings({
+    attributesToIndex: ['unordered(name)', 'unordered(alternativeNames)', 'unordered(description)', 'unordered(keywords)', 'unordered(filename)'],
+    customRanking: [ 'desc(github.stargazers_count)', 'asc(name)' ],
+    attributesForFaceting: ['fileType', 'keywords'],
+    optionalWords: ['js', 'css'] // those words are optional (jquery.colorbox.js <=> jquery.colorbox)
+  }, function(error, content) {
+    next();
+  });
+}
+
+function push(next) {
+  console.log('* Indexing ' + LIBRARIES.length + ' libraries');
+  index.addObjects(LIBRARIES, function(error, content) {
+    next();
+  });
+}
+
+function commit(next) {
+  console.log('* Moving index to production');
+  client.moveIndex('libraries.tmp', 'libraries', function(error, content) {
+    next();
+  });
+}
+
+//////
+////// Orchestrate it!
+//////
+async.series([
+  load,
+  authenticate,
+  crawl,
+  initIndex,
+  push,
+  commit
+], function(err) {
+  if (err) {
+    console.error(err);
     process.exit(1);
-  } else {
-    // atomic move to production index
-    client.moveIndex('libraries.tmp', 'libraries', function(error, content) {
-      process.exit(0);
-    });
   }
 });
