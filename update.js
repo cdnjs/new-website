e65c95f39e54dcb606da1096373934f9133867cc
Peter Dave Hello <hsu@peterdavehello.org>
2015-03-17 03:37:23 +0800

    update the update process to reflect the new work flow
    packages.json, packages.min.json, atom.xml and rss.xml will be built from the main repo
    
    We don't pull from aws for a long time

--- update.js
+++ update.js
@@ -5,58 +5,36 @@ var http = require('http');
 
 
 
-var packagesurl = 'https://s3.amazonaws.com/cdnjs-artifacts/packages.json?' + new Date().getTime();
-superagent.get(packagesurl, function(res, textStatus, xhr){
-  fs.writeFileSync('public/packages.json', JSON.stringify(res.body, null, 2), 'utf8');
-  fs.writeFileSync('public/packages.min.json', JSON.stringify(res.body), 'utf8');
+var pkgMeta = JSON.parse(fs.readFileSync('public/packages.min.json', 'utf8')).packages;
+// Generate sitemap
+
+var pages = [
+  'http://cdnjs.com/',
+  'http://cdnjs.com/about',
+  'http://cdnjs.com/login',
+  'http://cdnjs.com/register'
+];
 
 
-  // Generate sitemap
-
-  var pages = [
-    'http://cdnjs.com/',
-    'http://cdnjs.com/about',
-    'http://cdnjs.com/login',
-    'http://cdnjs.com/register'
-  ];
+var xml = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 
-  var xml = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+var librariePages = _.map(pkgMeta, function (package) {
+  return 'http://cdnjs.com/libraries/' + package.name;
+});
+pages = pages.concat(librariePages);
 
+var librarieNewsPages = _.map(pkgMeta, function (package) {
+  return 'http://cdnjs.com/libraries/' + package.name + '/news';
+});
 
-  var librariePages = _.map(res.body.packages, function (package) {
-    return 'http://cdnjs.com/libraries/' + package.name;
-  });
-  pages = pages.concat(librariePages);
+pages = pages.concat(librarieNewsPages);
 
-  var librarieNewsPages = _.map(res.body.packages, function (package) {
-    return 'http://cdnjs.com/libraries/' + package.name + '/news';
-  });
-
-  pages = pages.concat(librarieNewsPages);
-
-  _.each(pages, function(page){
-    xml += '<url><loc>' + page + '</loc></url>';
-  });
-
-
-  xml += '</urlset>';
-
-  fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
-
-
-
+_.each(pages, function(page){
+  xml += '<url><loc>' + page + '</loc></url>';
 });
 
 
+xml += '</urlset>';
 
-// I was rushing below r0fl
-var file = fs.createWriteStream("public/rss.xml");
-var request = http.get("http://s3.amazonaws.com/cdnjs-artifacts/rss", function(response) {
-  response.pipe(file);
-});
-
-var file2 = fs.createWriteStream("public/atom.xml");
-var request2 = http.get("http://s3.amazonaws.com/cdnjs-artifacts/rss", function(response) {
-  response.pipe(file2);
-});
+fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
