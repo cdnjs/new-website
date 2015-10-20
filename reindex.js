b089c232c79f62a1b33b6c5181b44344ef5cd7db
Peter Dave Hello <hsu@peterdavehello.org>
2015-10-20 19:56:49 +0800

    use async.eachLimit(8) instead of async.each in reindex

--- reindex.js
+++ reindex.js
@@ -74,7 +74,7 @@ function authenticate(next) {
 
 function crawl(gnext) {
   console.log('* Enriching libraries with GitHub');
-  async.each(LIBRARIES, function(library, next) {
+  async.eachLimit(LIBRARIES, 8, function(library, next) {
     var urls = [];
     // collect all repository urls
     if (library.repositories && _.isArray(library.repositories)) {
