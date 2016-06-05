f6f7f7b90f70364fd438006e43d47843ee2f177c
Peter Dave Hello <hsu@peterdavehello.org>
2016-06-05 21:38:45 +0800

    webServer use request variable

--- webServer.js
+++ webServer.js
@@ -151,7 +151,7 @@ function start() {
     }
 
     app.get('/request-new-lib', function(req, res) {
-        return res.redirect(302, 'https://github.com/cdnjs/cdnjs/issues/new?title=%5BRequest%5D%20Add%20_library_name_%20&body=**Library%20name%3A**%20%0A**Git%20repository%20url%3A**%0A**npm%20package%20url(optional)%3A**%20%0A**License(s)%3A**%0A**Official%20homepage%3A**%0A**Wanna%20say%20something?%20Leave%20message%20here%3A**%0A%0A=====================%0ANotes%20from%20cdnjs%20maintainer%3A%0APlease%20read%20the%20README.md%20and%20CONTRIBUTING.md%20document%20first.%0A%0AYou%20are%20welcome%20to%20add%20a%20library%20via%20sending%20pull%20request%2C%0Ait%27ll%20be%20faster%20then%20just%20opening%20a%20request%20issue%2C%0Aand%20please%20don%27t%20forget%20to%20read%20the%20guidelines%20for%20contributing%2C%20thanks!!');
+        return res.redirect(302, request);
     });
 
 
