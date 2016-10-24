05996ee7fdbb40fa8d93e2005135e7e8cecbdb4e
Peter Dave Hello <hsu@peterdavehello.org>
2016-10-24 12:50:16 +0800

    refactor part of copy button code

--- public/js/main.js
+++ public/js/main.js
@@ -52,13 +52,17 @@ function setFileURLs(new_provider) {
   var toggleButton = $('<button/>').attr('data-toggle', 'dropdown').attr('type', 'button').addClass('btn btn-primary btn-sm dropdown-toggle').append($('<span/>').addClass('caret'));
   copyElButton.appendTo(copyEl);
   toggleButton.appendTo(copyEl);
-  var SRIcopyButton = (typeof (SRI) !== "undefined") ? ('<li class="js"><a data-copy-embed="script-sri" data-copy-type="https:" class=" copy-https-script copy-button" href="#">Copy Script Tag with SRI</a></li>' +
-                    '<li class="css"><a data-copy-embed="link-sri" data-copy-type="https:" class=" copy-https-link copy-button" href="#">Copy Link Tag with SRI</a></li>') : '';
+  var SRIcopyButton = '';
+  if (typeof (SRI) !== "undefined") {
+    SRIcopyButton =
+      '<li class="js"><a data-copy-embed="script-sri" data-copy-type="https:" class="copy-https-script copy-button" href="#">Copy Script Tag with SRI</a></li>' +
+      '<li class="css"><a data-copy-embed="link-sri" data-copy-type="https:" class="copy-https-link copy-button" href="#">Copy Link Tag with SRI</a></li>';
+  }
   copyEl.append('<ul class="dropdown-menu copy-options">' +
-                    '<li><a data-copy-type="https:" class="copy-https-url copy-button" href="#">Copy Url</a></li>' +
-                    '<li class="js"><a data-copy-embed="script" data-copy-type="https:" class=" copy-https-script copy-button" href="#">Copy Script Tag</a></li>' +
-                    '<li class="css"><a data-copy-embed="link" data-copy-type="https:" class=" copy-https-link copy-button" href="#">Copy Link Tag</a></li>' + SRIcopyButton +
-                    '</ul>');
+                '<li><a data-copy-type="https:" class="copy-https-url copy-button" href="#">Copy Url</a></li>' +
+                '<li class="js"><a data-copy-embed="script" data-copy-type="https:" class=" copy-https-script copy-button" href="#">Copy Script Tag</a></li>' +
+                '<li class="css"><a data-copy-embed="link" data-copy-type="https:" class=" copy-https-link copy-button" href="#">Copy Link Tag</a></li>' +
+                SRIcopyButton + '</ul>');
 
   var copyContainer = $('<div/>');
   copyEl.attr('style', 'display: none;');
