439cf266207a0d921949c66158f7d8f83648279a
Peter Dave Hello <hsu@peterdavehello.org>
2017-02-21 06:01:00 +0800

    Update "request library" issue template

--- public/js/main.js
+++ public/js/main.js
@@ -223,16 +223,7 @@ function setFileURLs(new_provider) {
       '<br /><td class="text-center well" colspan="2">' +
       tempText + ' Would you like to ' +
       '<a href="' +
-        'https://github.com/cdnjs/cdnjs/issues/new?title=%5BRequest%5D%20Add%20' +
-        libraryName +
-        '%20&body=**Library%20name%3A**%20' +
-        libraryName +
-        '%0A**Git%20repository%20url%3A**%0A**npm%20package%20url(optional)%3A**%20%0A**' +
-        'License(s)%3A**%0A**Official%20homepage%3A**%0A**Wanna%20say%20something?' +
-        '%20Leave%20message%20here%3A**%0A%0A%0A%0A=====================%0ANotes%20from' +
-        '%20cdnjs%20maintainer%3A%0AYou%20are%20welcome%20to%20add%20a%20library%20via%20sending' +
-        '%20pull%20request%2C%0Ait%27ll%20be%20faster%20then%20just%20opening%20a%20request%20' +
-        'issue%2C%0Aand%20please%20don%27t%20forget%20to%20read%20the%20guidelines%20for%20contributing%2C%20thanks!!' +
+      'https://github.com/cdnjs/cdnjs/issues/new?title=%5BRequest%5D%20Add%20' + libraryName + '&body=**Library%20name%3A**%20' + libraryName + '%0A**Git%20repository%20url%3A**%20%0A**npm%20package%20name%20or%20url**%20(if%20there%20is%20one)%3A%20%0A**License%20(List%20them%20all%20if%20it%27s%20multiple)%3A**%20%0A**Official%20homepage%3A**%20%0A**Wanna%20say%20something%3F%20Leave%20message%20here%3A**%20%0A%0A%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%0A%0ANotes%20from%20cdnjs%20maintainer%3A%0APlease%20read%20the%20%5BREADME.md%5D(https%3A%2F%2Fgithub.com%2Fcdnjs%2Fcdnjs%23cdnjs-library-repository)%20and%20%5BCONTRIBUTING.md%5D(https%3A%2F%2Fgithub.com%2Fcdnjs%2Fcdnjs%2Fblob%2Fmaster%2F.github%2FCONTRIBUTING.md)%20document%20first.%0A%0AWe%20encourage%20you%20to%20add%20a%20library%20via%20sending%20pull%20request%2C%0Ait%27ll%20be%20faster%20than%20just%20opening%20a%20request%20issue%2C%0Asince%20there%20are%20tons%20of%20issues%2C%20please%20wait%20with%20patience%2C%0Aand%20please%20don%27t%20forget%20to%20read%20the%20guidelines%20for%20contributing%2C%20thanks!!%0A' +
         '" target="_blank">request it?</a>' +
         ' Please don\'t forget to <a href="https://github.com/cdnjs/cdnjs/issues?utf8=%E2%9C%93&q=' + libraryName + '" target="_blank">search if there is already an issue for it</a> before adding a request.' +
       '</td>';
