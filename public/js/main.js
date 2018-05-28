6c1c24c42cc7d1a507a3cf9746faabc4c632a853
Peter Dave Hello <hsu@peterdavehello.org>
2018-05-28 11:28:10 +0800

    [Website] Replace library request link with contribution guide

--- public/js/main.js
+++ public/js/main.js
@@ -291,7 +291,7 @@ function setFileURLs(new_provider) {
     var tempText2 =
       '<br /><td class="text-center well" colspan="2">' +
       tempText + '<br /> You can ' +
-      '<a href="' + getReqLibTmplUrl(content.query) + '" target="_blank">request it</a> if it fits our <a href="https://github.com/cdnjs/cdnjs/blob/master/CONTRIBUTING.md#a-issue" target="_blank"><strong>requirement</strong></a>.' +
+      '<a href="https://github.com/cdnjs/cdnjs/blob/master/CONTRIBUTING.md" target="_blank">contribute it</a> if it fits our <a href="https://github.com/cdnjs/cdnjs/blob/master/CONTRIBUTING.md#a-issue" target="_blank"><strong>requirement</strong></a>.' +
         ' Please don\'t forget to <a href="https://github.com/cdnjs/cdnjs/issues?utf8=%E2%9C%93&q=' + libraryName + '" target="_blank"><strong>search if there is already an issue for it</strong></a> before adding a request.' +
       '</td>';
     if (lazyScroll) {
