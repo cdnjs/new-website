a47683637b1d379efff734d7f89d1d41fb3919b8
Sven Sauleau <sven@cloudflare.com>
2020-02-03 12:07:47 +0000

    bump memory limits

--- runServer.sh
+++ runServer.sh
@@ -5,9 +5,9 @@ export WEB_CONCURRENCY=1
 wget -O public/packages.min.json https://storage.googleapis.com/cdnjs-assets/package.min.js
 
 if [ x"$APP" = x"mainSite" ]; then
-    node --expose-gc webServer.js
+    node --expose-gc --max-old-space-size=2048 webServer.js
 elif [ x"$APP" = x"api" ]; then
-    node --expose-gc apiServer.js
+    node --expose-gc --max-old-space-size=2048 apiServer.js
 else
     echo "Please provide valid \$APP variable: api or mainSite" 1>&2
     exit 1
